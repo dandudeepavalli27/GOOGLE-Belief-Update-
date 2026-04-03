7338ed83a6ef7dd3017cf66a9af7ed027ff1b649
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:44:42 +0530

    Create Belief Update.py

---
+++ Belief Update.py
@@ -0,0 +1,13 @@
+# SESSION 19 – Belief Update (Bayesian)
+
+# Step 1: Input values
+prior = 0.7
+observation = 0.6
+
+# Step 2: Compute updated belief
+belief = prior * observation
+
+# Step 3: Display result
+print("Updated Belief =", belief)
+
+print("\nProgram Executed Successfully")
